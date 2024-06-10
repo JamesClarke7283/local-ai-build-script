afcdf4db80568af78f3d05663a77dc1b575ae2e4
James Clarke <james@novora.ai>
2024-06-10 09:37:53 +0100

    Made some changes

--- scripts/compile.js
+++ scripts/compile.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env bun
 
 import { $ } from "bun";
-import { existsSync, chdir } from "fs";
+import { existsSync } from "fs";
 
 import { config } from "dotenv";
 // Load environment variables from .env file
@@ -17,8 +17,6 @@ if (!existsSync(repoPath)) {
     console.log('LocalAI directory already exists in src/, skipping clone.');
 }
 
-chdir(repoPath);
-
 // Ensure protoc is installed correctly and check its version
 try {
     const protocVersion = await $`protoc --version`.text();
@@ -28,9 +26,5 @@ try {
     process.exit(1);
 }
 
-// Set GO_TAGS and BUILD_TYPE
-process.env.GO_TAGS = 'p2p stablediffusion tts';
-process.env.BUILD_TYPE = 'cublas';
-
 // Build the project
-await $`make -j4 dist`;
+await $`make -j4 dist`.cwd(repoPath);
